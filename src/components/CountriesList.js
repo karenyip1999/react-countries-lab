046dca72152b2c5f5de3204954f9622dca8bd315
Karen Yip <karenyip1999@gmail.com>
2024-03-25 14:15:57 +0000

    Added CountriesList.js

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,6 +1,14 @@
 
-const CountriesList = () => {
+const CountriesList = ({country}) => {
+    const mappedCountry = country.map(c => {
+        return <Country c = {country} key = {country.id} />
+    })
+    return (
+        <>
+        {mappedCountry}
+        </>
 
+    )
 
 }
 
